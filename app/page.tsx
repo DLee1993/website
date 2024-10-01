bbcc333d4dc7732d1552507e7b64ab531c235095
David Lee <Dai180293@gmail.com>
2024-10-01 11:48:52 +0100

    add all hero text content, add additional y axis margins to mobile and tablet viewports

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
 export default function Home() {
     return (
-        <section>
-            <h1>hello world</h1>
+        <section className="my-14 md:my-16 lg:my-20">
+            <h1 className="text-heading font-Lora font-semibold">
+                I&apos;m David Lee, a detail-orientated frontend developer specialising in creating
+                engaging, user-friendly experiences.
+            </h1>
+            <p className="text-accent mt-8">
+                I enjoy building; finding a project that challenges my present way of thinking
+                allows me to grow and develop more effectively. My current obsession is
+                accessibility; I want everyone to have a consistent experience while visiting my
+                websites. I also create blog posts to strengthen my ability to communicate train of
+                thought.
+                <br />
+                <br />
+                Welcome to my digital personality.
+            </p>
         </section>
     );
 }
