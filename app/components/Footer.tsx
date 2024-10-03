4d02c3b4ced0ddee4f5ab3d4d5e2224d699fea7b
David Lee <Dai180293@gmail.com>
2024-10-03 07:32:47 +0100

    added all footer content and styling

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,15 @@
+import { scrollToTop } from "../hooks/ScrollToTop";
+import SocialLinks from "./SocialLinks";
 const Footer = () => {
-  return (
-    <footer className="customWidth">Footer</footer>
-  )
-}
-export default Footer
+    return (
+        <footer className="customWidth min-h-32 flex flex-col justify-evenly mt-48 mb-5">
+            <section className="flex justify-between items-center">
+                <SocialLinks />
+                <button onClick={scrollToTop}>back to top</button>
+            </section>
+            <div className="h-[2px] w-full bg-zinc-800"></div>
+            <p className="text-center text-accent  text-subText">Copyright 2024 | David Lee</p>
+        </footer>
+    );
+};
+export default Footer;
