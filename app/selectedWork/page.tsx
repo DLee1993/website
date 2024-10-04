950c975dbdf67e2c0a0926a7bce423d8ec68f8a0
David Lee <Dai180293@gmail.com>
2024-10-04 06:45:48 +0100

    added all content to root selected works page

--- app/selectedWork/page.tsx
+++ app/selectedWork/page.tsx
@@ -1,5 +1,53 @@
+"use client";
+
+import Link from "next/link";
+import { projects } from "../portfolioData";
+import Arrow from "../components/Arrow";
+import { motion } from "framer-motion";
+import { item, container } from "../lib/framerMotion";
+
 export default function SelectedWorkRootPage() {
-  return (
-    <div>work root page</div>
-  )
+    return (
+        <motion.section
+            variants={container}
+            initial="hidden"
+            animate="show"
+            className="my-14 md:my-16 lg:my-20"
+        >
+            <motion.article variants={item}>
+                <h1 className="text-subHeading font-medium">Selected Works</h1>
+                <p className="text-zinc-300 mt-6 text-subText sm:text-base">
+                    This is the accumulation of my learning, here you will find everything from full
+                    stack apps, landing pages and responsive components.
+                </p>
+            </motion.article>
+            <motion.section variants={item}>
+                <ul className="text-base flex flex-col gap-2 mt-5 sm:mt-7 md:mt-9 lg:mt-12">
+                    {projects.map((project, index) => (
+                        <li key={index} className="group">
+                            <Link
+                                href={`/selectedWork/${project.projectName}`}
+                                className="text-zinc-300 hover:bg-zinc-800/40 border-2 border-zinc-800 px-2 sm:px-4 py-4 flex justify-between items-center rounded-md transition-all duration-200"
+                            >
+                                <article className="w-11/12">
+                                    <p className="font-medium">{project.title}</p>
+                                    <p className="text-subText max-w-lg hidden min-[465px]:block">
+                                        {project.descriptionSnippet}
+                                    </p>
+                                </article>
+                                <Arrow />
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+                <Link
+                    href="https://github.com/DLee1993"
+                    target="_blank"
+                    className="flex justify-evenly items-center gap-2 customLinkHover w-48 h-10 text-subText mt-12 mx-auto ring-[1px] ring-zinc-300/10 group"
+                >
+                    view more on github
+                </Link>
+            </motion.section>
+        </motion.section>
+    );
 }
