f1d30bc7df118093659d0c84d52ffffacaa24274
David Lee <Dai180293@gmail.com>
2024-10-04 07:13:50 +0100

    added additional template data

--- app/portfolioData.js
+++ app/portfolioData.js
@@ -43,6 +43,133 @@ export const projects = [
         screenshot5: "/phone005.png",
         heroScreenShot: "/phone006.png",
     },
+    {
+        featured: true,
+        projectName: "index",
+        title: "Index: Your personal Organiser",
+        descriptionSnippet: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        fullDescription:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum",
+        projectType: "Full stack",
+        designSource: "www.youtube.com/youtubeChannel",
+        techStack: ["react", "next.js", "tailwind css"],
+        textSnippet1:
+            "This is where you add a brief description about the project and the reason for undertaking this project, keep it short and to the point. Do not blab on, it will only bore the reader!!",
+        textSnippet2:
+            "This is where you talk in depth about the technologies used and why you used them, do not be afraid to say you want to try something new and it did'nt work so you reverted back to a more familiar technology ( i.e. bootstrap to tailwind)",
+        textSnippet3:
+            "This is where you talk in depth about the potential for using different technology to what you used, this is going to show that you are capable of reflecting on whether your choice of framework or library is best suited to this project.",
+        laptopImage: "/laptop.png",
+        screenshot1: "/phone001.png",
+        screenshot2: "/phone002.png",
+        screenshot3: "/phone003.png",
+        screenshot4: "/phone004.png",
+        screenshot5: "/phone005.png",
+        heroScreenShot: "/phone006.png",
+    },
+    {
+        projectName: "mydoctor",
+        title: "My Doctor Healthcare app",
+        descriptionSnippet: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        fullDescription:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum",
+        projectType: "Full stack",
+        designSource: "www.youtube.com/youtubeChannel",
+        techStack: ["react", "next.js", "tailwind css"],
+        textSnippet1: "First snippet",
+        textSnippet2: "second snippet",
+        textSnippet3: "third snippet",
+        laptopImage: "/laptop.png",
+        screenshot1: "/phone003.png",
+        screenshot2: "/phone002.png",
+        screenshot3: "/phone004.png",
+        screenshot4: "/phone001.png",
+        screenshot5: "/phone005.png",
+        heroScreenShot: "/phone006.png",
+    },
+];
+
+export const blogs = [
+    {
+        featured: true,
+        blogName: "index",
+        title: "How to build a multi step form using React hook form",
+        descriptionSnippet:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel sint",
+        dateWritten: "feb 18, 2024",
+        readTime: "10",
+        textSnippet1:
+            "This is where you add a brief description about the project and the reason for undertaking this project, keep it short and to the point. Do not blab on, it will only bore the reader!!",
+        textSnippet2:
+            "This is where you talk in depth about the technologies used and why you used them, do not be afraid to say you want to try something new and it did'nt work so you reverted back to a more familiar technology ( i.e. bootstrap to tailwind)",
+        screenshot1: "/phone001.png",
+        screenshot2: "/phone002.png",
+        screenshot3: "/phone003.png",
+    },
+    {
+        featured: true,
+        blogName: "index",
+        title: "How to build a multi step form using React hook form",
+        descriptionSnippet:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel sint",
+        dateWritten: "feb 18, 2024",
+        readTime: "10",
+        textSnippet1:
+            "This is where you add a brief description about the project and the reason for undertaking this project, keep it short and to the point. Do not blab on, it will only bore the reader!!",
+        textSnippet2:
+            "This is where you talk in depth about the technologies used and why you used them, do not be afraid to say you want to try something new and it did'nt work so you reverted back to a more familiar technology ( i.e. bootstrap to tailwind)",
+        screenshot1: "/phone001.png",
+        screenshot2: "/phone002.png",
+        screenshot3: "/phone003.png",
+    },
+    {
+        featured: true,
+        blogName: "index",
+        title: "How to build a multi step form using React hook form",
+        descriptionSnippet:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel sint",
+        dateWritten: "feb 18, 2024",
+        readTime: "10",
+        textSnippet1:
+            "This is where you add a brief description about the project and the reason for undertaking this project, keep it short and to the point. Do not blab on, it will only bore the reader!!",
+        textSnippet2:
+            "This is where you talk in depth about the technologies used and why you used them, do not be afraid to say you want to try something new and it did'nt work so you reverted back to a more familiar technology ( i.e. bootstrap to tailwind)",
+        screenshot1: "/phone001.png",
+        screenshot2: "/phone002.png",
+        screenshot3: "/phone003.png",
+    },
+    {
+        featured: true,
+        blogName: "index",
+        title: "How to build a multi step form using React hook form",
+        descriptionSnippet:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel sint",
+        dateWritten: "feb 18, 2024",
+        readTime: "10",
+        textSnippet1:
+            "This is where you add a brief description about the project and the reason for undertaking this project, keep it short and to the point. Do not blab on, it will only bore the reader!!",
+        textSnippet2:
+            "This is where you talk in depth about the technologies used and why you used them, do not be afraid to say you want to try something new and it did'nt work so you reverted back to a more familiar technology ( i.e. bootstrap to tailwind)",
+        screenshot1: "/phone001.png",
+        screenshot2: "/phone002.png",
+        screenshot3: "/phone003.png",
+    },
+    {
+        featured: true,
+        blogName: "index",
+        title: "How to build a multi step form using React hook form",
+        descriptionSnippet:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel sint",
+        dateWritten: "feb 18, 2024",
+        readTime: "10",
+        textSnippet1:
+            "This is where you add a brief description about the project and the reason for undertaking this project, keep it short and to the point. Do not blab on, it will only bore the reader!!",
+        textSnippet2:
+            "This is where you talk in depth about the technologies used and why you used them, do not be afraid to say you want to try something new and it did'nt work so you reverted back to a more familiar technology ( i.e. bootstrap to tailwind)",
+        screenshot1: "/phone001.png",
+        screenshot2: "/phone002.png",
+        screenshot3: "/phone003.png",
+    },
 ];
 
 export const resume = [
@@ -91,24 +218,5 @@ export const resume = [
     {
         title: "Personal Interests",
         keynotes: ["Kickboxing, I have found a sport that pushes me  beyond my limits"],
-    },
-];
-
-export const blogs = [
-    {
-        featured: true,
-        blogName: "index",
-        title: "How to build a multi step form using React hook form",
-        descriptionSnippet:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel sint",
-        dateWritten: "feb 18, 2024",
-        readTime: "10",
-        textSnippet1:
-            "This is where you add a brief description about the project and the reason for undertaking this project, keep it short and to the point. Do not blab on, it will only bore the reader!!",
-        textSnippet2:
-            "This is where you talk in depth about the technologies used and why you used them, do not be afraid to say you want to try something new and it did'nt work so you reverted back to a more familiar technology ( i.e. bootstrap to tailwind)",
-        screenshot1: "/phone001.png",
-        screenshot2: "/phone002.png",
-        screenshot3: "/phone003.png",
     },
 ];
