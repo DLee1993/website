b7cf6c25d6b32d0451ae9d76cee6bd3f35ebee2b
David Lee <Dai180293@gmail.com>
2024-10-04 02:30:37 +0100

    adjusted the tooltips to only be available on desktop viewports

--- app/components/SocialLinks.tsx
+++ app/components/SocialLinks.tsx
@@ -15,7 +15,7 @@ export default function SocialLinks() {
                 </Link>
                 <div
                     role="tooltip"
-                    className="text-subText absolute -top-10 left-0 z-10 py-2 px-3 text-sm font-medium text-foreground bg-zinc-800 rounded-sm shadow-sm opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300"
+                    className="hidden lg:inline-block text-subText absolute -top-10 left-0 z-10 py-2 px-3 text-sm font-medium text-foreground bg-zinc-800 rounded-sm shadow-sm opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300"
                 >
                     Github
                 </div>
@@ -31,7 +31,7 @@ export default function SocialLinks() {
                 </Link>
                 <div
                     role="tooltip"
-                    className="text-subText absolute -top-10 left-0 z-10 py-2 px-3 text-sm font-medium text-foreground bg-zinc-800 rounded-sm shadow-sm opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300"
+                    className="hidden lg:inline-block text-subText absolute -top-10 left-0 z-10 py-2 px-3 text-sm font-medium text-foreground bg-zinc-800 rounded-sm shadow-sm opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300"
                 >
                     Discord
                 </div>
@@ -47,7 +47,7 @@ export default function SocialLinks() {
                 </Link>
                 <div
                     role="tooltip"
-                    className="w-[137px] text-subText absolute -top-10 left-0 z-10 py-2 px-3 text-sm font-medium text-foreground bg-zinc-800 rounded-sm shadow-sm opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300"
+                    className="hidden lg:inline-block w-[137px] text-subText absolute -top-10 left-0 z-10 py-2 px-3 text-sm font-medium text-foreground bg-zinc-800 rounded-sm shadow-sm opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300"
                 >
                     Frontend mentor
                 </div>
