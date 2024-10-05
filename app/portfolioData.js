c574ede7b299bde92da85f8a1637b21e20f8406f
David Lee <Dai180293@gmail.com>
2024-10-05 19:39:59 +0100

    added additional data to blog and restructured data structure for ease of use on the blog page

--- app/portfolioData.js
+++ app/portfolioData.js
@@ -54,83 +54,39 @@ export const projects = [
 export const blogs = [
     {
         featured: true,
-        blogName: "index",
-        title: "How to build a multi step form using React hook form",
-        descriptionSnippet:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel sint",
         dateWritten: "feb 18, 2024",
-        readTime: "10",
-        textSnippet1:
-            "This is where you add a brief description about the project and the reason for undertaking this project, keep it short and to the point. Do not blab on, it will only bore the reader!!",
-        textSnippet2:
-            "This is where you talk in depth about the technologies used and why you used them, do not be afraid to say you want to try something new and it did'nt work so you reverted back to a more familiar technology ( i.e. bootstrap to tailwind)",
+        title: "How to build a multi step form using React hook form",
         screenshot1: "/phone001.png",
         screenshot2: "/phone002.png",
         screenshot3: "/phone003.png",
+        textContent: {
+            descriptionSnippet:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel sint Lorem ipsum dolor sit amet consectetur adipisicing elit. This is where you talk in depth about the technologies used and why you used them, do not be afraid to say you want to try something new and it did'nt work so you reverted back to a more familiar technology ( i.e. bootstrap to tailwind )",
+            textSnippet1:
+                "This is where you add a brief description about the project and the reason for undertaking this project, keep it short and to the point. Do not blab on, it will only bore the reader!!",
+            textSnippet2:
+                "This is where you talk in depth about the technologies used and why you used them, do not be afraid to say you want to try something new and it did'nt work so you reverted back to a more familiar technology ( i.e. bootstrap to tailwind )",
+            textSnippet3:
+                " Lorem ipsum dolor sit amet, consectetueradipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum",
+        },
     },
     {
         featured: true,
-        blogName: "index",
-        title: "How to build a multi step form using React hook form",
-        descriptionSnippet:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel sint",
-        dateWritten: "feb 18, 2024",
-        readTime: "10",
-        textSnippet1:
-            "This is where you add a brief description about the project and the reason for undertaking this project, keep it short and to the point. Do not blab on, it will only bore the reader!!",
-        textSnippet2:
-            "This is where you talk in depth about the technologies used and why you used them, do not be afraid to say you want to try something new and it did'nt work so you reverted back to a more familiar technology ( i.e. bootstrap to tailwind)",
+        dateWritten: "jun 22, 2000",
+        title: "Let's build an animated filter component",
         screenshot1: "/phone001.png",
         screenshot2: "/phone002.png",
         screenshot3: "/phone003.png",
-    },
-    {
-        featured: true,
-        blogName: "index",
-        title: "How to build a multi step form using React hook form",
-        descriptionSnippet:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel sint",
-        dateWritten: "feb 18, 2024",
-        readTime: "10",
-        textSnippet1:
-            "This is where you add a brief description about the project and the reason for undertaking this project, keep it short and to the point. Do not blab on, it will only bore the reader!!",
-        textSnippet2:
-            "This is where you talk in depth about the technologies used and why you used them, do not be afraid to say you want to try something new and it did'nt work so you reverted back to a more familiar technology ( i.e. bootstrap to tailwind)",
-        screenshot1: "/phone001.png",
-        screenshot2: "/phone002.png",
-        screenshot3: "/phone003.png",
-    },
-    {
-        featured: true,
-        blogName: "index",
-        title: "How to build a multi step form using React hook form",
-        descriptionSnippet:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel sint",
-        dateWritten: "feb 18, 2024",
-        readTime: "10",
-        textSnippet1:
-            "This is where you add a brief description about the project and the reason for undertaking this project, keep it short and to the point. Do not blab on, it will only bore the reader!!",
-        textSnippet2:
-            "This is where you talk in depth about the technologies used and why you used them, do not be afraid to say you want to try something new and it did'nt work so you reverted back to a more familiar technology ( i.e. bootstrap to tailwind)",
-        screenshot1: "/phone001.png",
-        screenshot2: "/phone002.png",
-        screenshot3: "/phone003.png",
-    },
-    {
-        featured: true,
-        blogName: "index",
-        title: "How to build a multi step form using React hook form",
-        descriptionSnippet:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel sint",
-        dateWritten: "feb 18, 2024",
-        readTime: "10",
-        textSnippet1:
-            "This is where you add a brief description about the project and the reason for undertaking this project, keep it short and to the point. Do not blab on, it will only bore the reader!!",
-        textSnippet2:
-            "This is where you talk in depth about the technologies used and why you used them, do not be afraid to say you want to try something new and it did'nt work so you reverted back to a more familiar technology ( i.e. bootstrap to tailwind)",
-        screenshot1: "/phone001.png",
-        screenshot2: "/phone002.png",
-        screenshot3: "/phone003.png",
+        textContent: {
+            descriptionSnippet:
+                "Lorem ipsum dolor sit amet, consectetueradipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum",
+            textSnippet1:
+                "This is where you add a brief description about the project and the reason for undertaking this project, keep it short and to the point. Do not blab on, it will only bore the reader!!, This is where you add a brief description about the project and the reason for undertaking this project, keep it short and to the point. Do not blab on, it will only bore the reader!!",
+            textSnippet2:
+                " Lorem ipsum dolor sit amet, consectetueradipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum",
+            textSnippet3:
+                " Lorem ipsum dolor sit amet, consectetueradipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum",
+        },
     },
 ];
 
